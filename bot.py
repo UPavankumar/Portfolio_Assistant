1c7f4a6c70a78e25abe702f419ed95cd1136af2c
U Pavan Kumar <74598153+UPavankumar@users.noreply.github.com>
2025-04-27 16:42:52 +0530

    removed dotenv

--- bot.py
+++ bot.py
@@ -1,11 +1,7 @@
 import streamlit as st
 from groq import Groq
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-api_key = os.getenv("GROQ_API_KEY")
+api_key = st.secrets["GROQ_API_KEY"]
 
 client = Groq(api_key=api_key)
 
